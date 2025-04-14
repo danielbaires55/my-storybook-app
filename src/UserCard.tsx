f01b6ba7c0b7d40b413397ed3fd88486c88d342c
danielbaires55 <baires468@gmail.com>
2025-04-14 09:47:40 +0200

    feat: add basic components

---
+++ src/UserCard.tsx
@@ -0,0 +1,14 @@
+
+type UserCardProps = {
+  name: string;
+  email: string;
+};
+
+export const UserCard: React.FC<UserCardProps> = ({ name, email }) => {
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '1rem', borderRadius: '5px' }}>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+}
